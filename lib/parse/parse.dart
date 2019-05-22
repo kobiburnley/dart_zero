5638da2567fd5477f060a406636ab9a96c69c0bd
Kobi <qobyyyy@gmail.com>
2019-05-22 23:50:33 +0300

    dart_zero

---
+++ lib/parse/parse.dart
@@ -0,0 +1,34 @@
+Iterable<T> parseIterable<T>(dynamic list, T fn(dynamic o), {Iterable<T> orElse()}) {
+  if (list is Iterable) {
+    return list.map(fn);
+  }
+  return orElse() ?? [];
+}
+
+List<T> parseList<T>(dynamic list, T fn(dynamic o), {Iterable<T> orElse()}) {
+  if (list is Iterable) {
+    return list.map(fn).toList();
+  }
+  return orElse() ?? [];
+}
+
+String parseString(dynamic s, {String orElse = ''}) {
+  if (s is String) {
+    return s;
+  }
+  return orElse;
+}
+
+int parseInt(dynamic n, {int orElse = 0}) {
+  if (n is int) {
+    return n;
+  }
+  return orElse;
+}
+
+num parseNum(dynamic n, {num orElse = 0}) {
+  if (n is num) {
+    return n;
+  }
+  return orElse;
+}
