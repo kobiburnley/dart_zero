6debf23dcce192941b1153664eced1ea927793bd
Kobi <qobyyyy@gmail.com>
2020-02-10 22:25:14 +0200

    feat: parseMap

--- lib/parse/parse.dart
+++ lib/parse/parse.dart
@@ -1,4 +1,5 @@
-Iterable<T> parseIterable<T>(dynamic list, T fn(dynamic o), {Iterable<T> orElse()}) {
+Iterable<T> parseIterable<T>(dynamic list, T fn(dynamic o),
+    {Iterable<T> orElse()}) {
   if (list is Iterable) {
     return list.map(fn);
   }
@@ -10,6 +11,52 @@ List<T> parseList<T>(dynamic list, T fn(dynamic o), {Iterable<T> orElse()}) {
     return list.map(fn).toList();
   }
   return orElse?.call() ?? [];
+}
+
+Map<K, V> parseMap<K, V>(
+  dynamic raw, {
+  K Function(V value, dynamic key) parseKey,
+  V parseValue(dynamic v),
+  Map<K, V> orElse(),
+}) {
+  if (raw is Map) {
+    return Map.fromEntries(
+      raw.entries.map(
+        (entry) => parseMapEntry(
+          entry,
+          parseKey: parseKey,
+          parseValue: parseValue,
+        ),
+      ),
+    );
+  }
+
+  if (raw is List) {
+    final values = raw.map(parseValue);
+    return Map.fromIterables(
+      values.map((value) => parseKey(value, null)),
+      values,
+    );
+  }
+  return orElse?.call() ?? {};
+}
+
+MapEntry<K, V> parseMapEntry<K, V>(
+  dynamic entry, {
+      K Function(V value, dynamic key) parseKey,
+  V parseValue(dynamic v),
+  MapEntry<K, V> orElse(),
+}) {
+  if (entry is MapEntry) {
+    final value = parseValue(entry.value);
+    return MapEntry(
+      parseKey(value, entry.key),
+      value,
+    );
+  }
+  final value = parseValue(null);
+
+  return orElse?.call() ?? MapEntry(parseKey(value, null), value);
 }
 
 String parseString(dynamic s, {String orElse = ''}) {
